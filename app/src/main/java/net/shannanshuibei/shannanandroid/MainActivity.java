9725dd7e7042a32e63aaacd98c821cfe798e7fe6
山南 <zhangpeichaook@163.com>
2016-04-28 00:11:29 +0800

    tab

--- app/src/main/java/net/shannanshuibei/shannanandroid/MainActivity.java
+++ app/src/main/java/net/shannanshuibei/shannanandroid/MainActivity.java
@@ -1,7 +1,10 @@
 package net.shannanshuibei.shannanandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_tab_viewpager = (Button) findViewById(R.id.btn_tab_viewpager);
+        btn_tab_viewpager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TabViewpagerActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
     }
 }
