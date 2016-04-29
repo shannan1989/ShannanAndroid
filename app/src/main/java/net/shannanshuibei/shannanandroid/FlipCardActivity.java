d56ec96fa61057a72d1d206f54bbfda40cd4596f
山南 <zhangpeichaook@163.com>
2016-04-29 14:45:53 +0800

    flip card

--- app/src/main/java/net/shannanshuibei/shannanandroid/FlipCardActivity.java
+++ app/src/main/java/net/shannanshuibei/shannanandroid/FlipCardActivity.java
@@ -4,25 +4,44 @@ import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class FlipCardActivity extends AppCompatActivity {
 
-    FrameLayout mFlContainer = (FrameLayout) findViewById(R.id.main_fl_container);
+    @BindView(R.id.main_fl_container)
+    FrameLayout mFlContainer;
+
+    @BindView(R.id.main_fl_card_front)
+    FrameLayout mFlCardFront;
+
+    @BindView(R.id.main_fl_card_back)
+    FrameLayout mFlCardBack;
 
     private AnimatorSet mRightOutSet;
     private AnimatorSet mLeftInSet;
+
+    private boolean mIsShowBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_card);
 
+        ButterKnife.bind(this);
+
         this.setAnimators();
+        setCameraDistance();
     }
 
+    /**
+     * 设置动画
+     */
     private void setAnimators() {
         mRightOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_flip_out);
         mLeftInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_flip_in);
@@ -43,5 +62,36 @@ public class FlipCardActivity extends AppCompatActivity {
                 mFlContainer.setClickable(true);
             }
         });
+    }
+
+    /**
+     * 改变视角距离，贴近屏幕
+     */
+    private void setCameraDistance() {
+        int distance = 16000;
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mFlCardFront.setCameraDistance(scale);
+        //mFlCardBack.setCameraDistance(scale);
+    }
+
+    /**
+     * 翻转卡片
+     *
+     * @param view View
+     */
+    public void flipCard(View view) {
+        if (!mIsShowBack) {//正面朝上
+            mRightOutSet.setTarget(mFlCardFront);
+            mLeftInSet.setTarget(mFlCardBack);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = true;
+        } else {//背面朝上
+            mRightOutSet.setTarget(mFlCardBack);
+            mLeftInSet.setTarget(mFlCardFront);
+            mRightOutSet.start();
+            mLeftInSet.start();
+            mIsShowBack = true;
+        }
     }
 }
