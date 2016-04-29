762c945bc94e07d30489fea2daadebd236e2357e
山南 <zhangpeichaook@163.com>
2016-04-29 11:39:49 +0800

    flip card

--- app/src/main/java/net/shannanshuibei/shannanandroid/FlipCardActivity.java
+++ app/src/main/java/net/shannanshuibei/shannanandroid/FlipCardActivity.java
@@ -1,13 +1,47 @@
 package net.shannanshuibei.shannanandroid;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class FlipCardActivity extends AppCompatActivity {
+
+    FrameLayout mFlContainer = (FrameLayout) findViewById(R.id.main_fl_container);
+
+    private AnimatorSet mRightOutSet;
+    private AnimatorSet mLeftInSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flip_card);
+
+        this.setAnimators();
+    }
+
+    private void setAnimators() {
+        mRightOutSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_flip_out);
+        mLeftInSet = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.anim_flip_in);
+
+        mRightOutSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+
+                mFlContainer.setClickable(false);
+            }
+        });
+        mLeftInSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+
+                mFlContainer.setClickable(true);
+            }
+        });
     }
 }
