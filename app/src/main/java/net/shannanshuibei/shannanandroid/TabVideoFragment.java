65a688891bfc44fb52b7524de9de1a11ce901e15
山南 <zhangpeichaook@163.com>
2016-05-03 23:02:31 +0800

    tab by fragment

---
+++ app/src/main/java/net/shannanshuibei/shannanandroid/TabVideoFragment.java
@@ -0,0 +1,19 @@
+package net.shannanshuibei.shannanandroid;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabVideoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.view_4_tab_video, container, false);
+    }
+
+}
